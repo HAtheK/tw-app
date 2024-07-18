2c9f82b458e445db85af9dade807b5fd533264ee
HAtheK <thekha@naver.com>
2024-07-18 16:29:28 +0900

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -35,7 +35,7 @@ export default function RootLayout({
               border-radius: 8px;
               box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
           }      
-    </style>
+        </style>
       </head>
       <body>{children}</body>
     </html>
