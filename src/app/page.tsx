849742ca47984c713e67a50c62cd85edf85907c3
HAtheK <thekha@naver.com>
2024-07-18 17:30:07 +0900

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,32 +12,21 @@ export async function generateMetadata(): Promise<Metadata> {
     
   // 오늘 날짜를 가져오기
   const today = new Date();
-
   
   // 두 날짜의 밀리초 차이 계산
   const differenceInMilliseconds = +today - +startDate;
-  console.log('differenceInMilliseconds:' + differenceInMilliseconds);
+
   // 하루의 밀리초
   const oneDayInMilliseconds = 24 * 60 * 60 * 1000;
   // 밀리초를 일 단위로 변환
   const differenceInDays = Math.floor(differenceInMilliseconds / oneDayInMilliseconds);
-  console.log('differenceInDays:' + differenceInDays);
+
   const period= differenceInDays % 2;
-  
   const ogImage = images[period];
   
-  
-  const format = useFormatter();
-  // Renders "Nov 20, 2020"
-  format.dateTime(today, {
-    year: 'numeric',
-    month: 'short',
-    day: 'numeric'
-  });
-  
   const rss = [
-    today + "오늘의 자원지는 기름(Oil Field)",  // 나머지가 0일 때 사용할 이미지
-    today + "오늘의 자원지는 옥수수(Farmland)"   // 나머지가 1일 때 사용할 이미지
+    "자원지:기름(Oil Field)",  // 나머지가 0일 때 사용할 이미지
+    "자원지:옥수수(Farmland)"   // 나머지가 1일 때 사용할 이미지
   ];
   
   const ogRss = rss[period];
@@ -46,8 +35,8 @@ export async function generateMetadata(): Promise<Metadata> {
     title: 'Open Graph Dynamic Image',
     description: `This page dynamically changes the Open Graph image based on the date. Days difference: ${differenceInDays}`,
     openGraph: {
-      title: '3407신 RSS',
-      description: ogRss,
+      title: '신³⁴⁰⁷'+ogRss,
+      description: '기준:'+ today,
       images: ogImage
     }
   };
