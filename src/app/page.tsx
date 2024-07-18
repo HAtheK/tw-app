c329b3c85070ff2028e8206f8eaa669df6c71e1d
HAtheK <thekha@naver.com>
2024-07-18 09:43:59 +0900

    modified:   src/app/page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,15 +12,18 @@ export async function generateMetadata(): Promise<Metadata> {
     
   // 특정 날짜를 설정 (예: 2024-07-01)
   const startDate = new Date('2024-07-18');
-    
+  console.log('startDate:' + startDate);  
   // 오늘 날짜를 가져오기
   const today = new Date();
+  console.log('today:' + today);
   // 두 날짜의 밀리초 차이 계산
   const differenceInMilliseconds = +today - +startDate;
+  console.log('differenceInMilliseconds:' + differenceInMilliseconds);
   // 하루의 밀리초
   const oneDayInMilliseconds = 24 * 60 * 60 * 1000;
   // 밀리초를 일 단위로 변환
   const differenceInDays = Math.floor(differenceInMilliseconds / oneDayInMilliseconds);
+  console.log('differenceInDays:' + differenceInDays);
   const period= differenceInDays % 2;
   
   const ogImage = images[period];
