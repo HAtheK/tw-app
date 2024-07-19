be15ffa0d8864d3267ef38dd3dc1f8277ca3e1f6
HAtheK <thekha@naver.com>
2024-07-19 11:02:10 +0900

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,22 +7,35 @@ export async function generateMetadata(): Promise<Metadata> {
     ];
 
     
-  // 특정 날짜를 설정 (예: 2024-07-01)
+   // 특정 날짜를 설정 (예: 2024-07-01)
   const startDate = new Date('2024-07-16');
     
   // 오늘 날짜를 가져오기
   const today = new Date();
-  
-  // 두 날짜의 밀리초 차이 계산
-  const differenceInMilliseconds = +today - +startDate;
 
+
+  // UTC 시간 계산
+  const startDateUTC = startDate.getTime() + (startDate.getTimezoneOffset() * 60 * 1000);
+  const todayUTC = today.getTime() + (today.getTimezoneOffset() * 60 * 1000);
+
+  // UTC to KST (UTC + 9시간)
+  const KR_TIME_DIFF = 9 * 60 * 60 * 1000;
+  const startDateKST = new Date(startDateUTC + (KR_TIME_DIFF));
+  const todayKST = new Date(todayUTC + (KR_TIME_DIFF));
+  document.writeln("한국시간s : " + todayKST+"<BR>");
+  document.writeln("한국시간n : " + todayKST+"<BR>");
+
+  // 두 날짜의 밀리초 차이 계산
+  const differenceInMilliseconds = +todayKST - +startDateKST;
+  document.writeln("시간차 : " + differenceInMilliseconds+"<BR>");
   // 하루의 밀리초
   const oneDayInMilliseconds = 24 * 60 * 60 * 1000;
   // 밀리초를 일 단위로 변환
   const differenceInDays = Math.floor(differenceInMilliseconds / oneDayInMilliseconds);
-
+  document.writeln("일차 : " + differenceInDays+"<BR>");
   const period= differenceInDays % 2;
   const ogImage = images[period];
+  document.writeln("인덱스 : " + period+"<BR>");
   
   const rss = [
     "자원지:기름(Oil Field)",  // 나머지가 0일 때 사용할 이미지
