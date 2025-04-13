47702236332d451d767cd28d7d40fdf03b528ff5
HAtheK <thekha@naver.com>
2025-04-14 02:52:05 +0900

    new file:   src/lib/session.ts 	new file:   src/lib/supabase.ts

---
+++ src/lib/session.ts
@@ -0,0 +1,13 @@
+// src/lib/session.ts
+'use server';
+
+import { createServerComponentClient } from '@supabase/auth-helpers-nextjs';
+import { cookies } from 'next/headers';
+
+export async function getUserSession() {
+  const supabase = createServerComponentClient({ cookies });
+  const {
+    data: { session },
+  } = await supabase.auth.getSession();
+  return session;
+}
