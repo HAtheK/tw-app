0e34d33072a7c58778a8cbd56c7ee174fce0cd73
HAtheK <thekha@naver.com>
2025-04-20 14:05:33 +0900

    modified:   src/app/page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -79,7 +79,7 @@ const KakaoSharePage = () => {
           </div>
         </div>
         <div className="w-full flex justify-center items-center px-4">
-          <button className="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded">
+          <button className="flex items-center justify-center gap-2 bg-transparent hover:bg-gray-100 text-white px-4 py-2 rounded-full text-sm">
             Button
           </button>
         </div>
