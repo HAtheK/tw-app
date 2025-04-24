de7349debc3493925dac7d92e9342e234b67d8f1
HAtheK <thekha@naver.com>
2025-04-25 02:56:06 +0900

    modified:   src/components/ShareClient.tsx

--- src/components/ShareClient.tsx
+++ src/components/ShareClient.tsx
@@ -117,6 +117,24 @@ export default function ShareClient({ userId, nickname, kakaoId }: ShareClientPr
             >
       
         <div className="w-full mt-8">
+          
+          <div className="flex justify-between items-center mb-2">
+              <h3 className="text-lg font-semibold">🎬 롯데멤버스 카드 마케팅 담당자</h3>
+          </div>
+
+          <div className="w-full max-w-md aspect-video mx-auto">
+            <iframe
+              className="w-full h-full rounded-xl shadow-md"
+              src="https://www.youtube.com/embed/RAZRIYDdVRw?si=_r10oLv1kyEJNJAB"
+              title="YouTube video player"
+              frameBorder="0"
+              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+              referrerPolicy="strict-origin-when-cross-origin"
+              allowFullScreen
+            ></iframe>
+          </div>
+          <div style={{ width: '100%', minHeight: '1px', height: '30px' }}></div>
+
           <div className="flex justify-between items-center mb-2">
             <h3 className="text-lg font-semibold">🏆 TOP10 공유 순위</h3>
             <button onClick={fetchTop10} title="새로고침">
