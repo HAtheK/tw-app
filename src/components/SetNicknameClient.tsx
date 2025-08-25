4606106dcec8ce52e9d0ee7282c778e6160fa872
HAtheK <thekha@naver.com>
2025-08-25 10:41:16 +0900

    modified:   src/components/SetNicknameClient.tsx

--- src/components/SetNicknameClient.tsx
+++ src/components/SetNicknameClient.tsx
@@ -60,12 +60,21 @@ const SetNicknameClient = ({ userId }: Props) => {
         </header>
       <main className="flex flex-col gap-6 items-center mt-24 relative z-10">
       {/* 콘텐츠 */}
+      <p style={{ textAlign: 'center', lineHeight: 1, marginTop: '6px' }}>
+              <span style={{ fontSize: '36px' }}>
+                <span style={{ color: 'rgb(255, 255, 255)' }}>
+                  <strong>Challenge Nickname</strong>
+                </span>
+              </span>
+            </p>
       <Image src="/nick.png" alt="로고" width={100} height={100} />
       <div style={{ width: '100%', minHeight: '1px', height: '60px' }}></div>
-      <section className="flex flex-col items-center justify-center space-y-6">
-        <div style={{width:'100%', minHeight:'1px',height:'60px'}}></div>
-        <div className="p-4 w-full max-w-md">
-          <h1 className="text-xl font-bold mb-4">닉네임을 입력해주세요</h1>
+      <p className="text-xl leading-relaxed font-medium">
+          <span style={{ color: 'rgb(255, 255, 255)' }}>
+          챌린지에서 사용할 닉네임을 입력해주세요
+          </span>
+        </p>
+        {/* 닉네임 입력창 및 설정 버튼 */}
           <input
             type="text"
             value={nickname}
@@ -80,17 +89,17 @@ const SetNicknameClient = ({ userId }: Props) => {
           >
             등록하고 시작하기
           </button>
-        </div>
+        
         <div style={{width:'100%', minHeight:'1px',height:'20px'}}></div>
         <p className="text-xs text-left px-4">
             ※ 닉네임은 자신을 나타내는 이름과 같습니다.<br/>
             ※ 불벅적이거나 타인에게 오해를 일으키고 불쾌감을 주는 닉네임은 변경/삭제될 수 있습니다.<br/>
             ※ 닉네임음 최초 설정 하신 후, 변경할 수 없습니다.</p>
-      </section>
+      
 
       {/* 푸터 */}
       <footer className="fixed bottom-0 left-0 w-full h-[30px] text-white flex items-center justify-center font-ptd">
-        임직원 이용 목적으로 제작한 사이트 입니다. 문의사항은 언제든 PLCC셀로 연락주세요.
+        임직원 이용 목적으로 제작한 사이트 입니다.
       </footer>
     </main>
   </div>
