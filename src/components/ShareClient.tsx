c63bc5400a95531ba8a03dd8423dff9fd423cd6b
HAtheK <thekha@naver.com>
2025-08-25 16:34:49 +0900

    modified:   src/components/ShareClient.tsx

--- src/components/ShareClient.tsx
+++ src/components/ShareClient.tsx
@@ -113,9 +113,8 @@ export default function ShareClient({ userId, nickname, kakaoId }: ShareClientPr
           <Image
             src="/persons.png"
             alt=""
-            width={472} // 고정 px이 아닌, 최대 사이즈 제어
-            height={467}
-            className="object-contain mt-28px"
+            width={470} // 고정 px이 아닌, 최대 사이즈 제어
+            className="object-contain relative top-[24px]"
           />
         </div>
         </section>
