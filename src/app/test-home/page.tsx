79472e5142557a175ad1ad1561ad250d37b76a4d
HAtheK <thekha@naver.com>
2025-11-15 20:04:36 +0900

    modified:   src/app/test-home/page.tsx

--- src/app/test-home/page.tsx
+++ src/app/test-home/page.tsx
@@ -82,18 +82,21 @@ export default function TestHome() {
         >
       <div className="text-table py-6">
         <p style={{textAlign: 'center'}}>
-          <span style={{color: 'rgb(255, 95, 56)', fontSize: '13px'}}>
+          <span style={{color: 'rgb(255, 95, 56)', fontSize: '16px'}}>
             <strong>사이트 소개</strong>
           </span>
         </p>
         <p style={{textAlign: 'center',marginTop:'16px'}}>
           <span style={{color: 'rgb(255, 255, 255)', fontSize: '25px'}}>
-            <strong>롯데멤버스의 소식을<br/>조금 더 편하게, 많이 알리기 위해<br/>제작하였습니다.</strong>
+            <strong>간단한 질문을 통해 나와 성향이<br/>가장 잘 맞는 </strong>
+          </span>
+          <span style={{color: 'rgba(255, 168, 87, 1)', fontSize: '25px'}}>
+            <strong>서비스를 찾아보세요<br/></strong>
           </span>
         </p>
         <p style={{textAlign: 'center', lineHeight: '1.15', marginTop:'6px'}}>
           <span style={{color: 'rgb(141, 141, 147)', fontSize: '13px'}}>
-            불편하고 번거롭지 않게 "롯데멤버스의 소식"을 알릴 수 있습니다.<br/><b>공유하기 챌린지 참여</b>하여 TOP10이 되어 보세요!
+            L.POINT APP, L.PAY, 포인트 충전결제, 롯데멤버스카드, 외국인 카드 등의<br/> "롯데멤버스의 서비스"를 안내합니다.<br/>
           </span>
         </p>
       </div>
